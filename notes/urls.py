557783777ed80d27d4e95730fc0a939141e733f5
raflir13 <rafli.ramdhani1313@gmail.com>
2025-12-24 11:18:29 +0700

    update urls notes

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import NoteViewSet, note_list_html
 
 router = DefaultRouter()
+router.register(r'notes', NoteViewSet, basename='note')
 
 urlpatterns = [
     path('', include(router.urls)),
